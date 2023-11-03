c05487e50041707395a05c7df6f82282956f0dc8
Lucia Hadbavniková <88892574+glooneltharion@users.noreply.github.com>
2023-11-03 08:46:41 +0100

    star: change function parameter, add outline color

--- star.py
+++ star.py
@@ -1,16 +1,16 @@
 import tkinter as tk
 
 
-def draw_star(star_color, bg_color):
+def draw_star(star_color, bg_color, outline_color ):
     root = tk.Tk()
 
     canvas = tk.Canvas(root, width=400, height=420, bg=bg_color)
     canvas.pack()
 
     points = [200, 20, 80, 396, 380, 156, 20, 156, 320, 396]
-    canvas.create_polygon(points, outline=star_color, fill=star_color)
+    canvas.create_polygon(points, outline=outline_color, fill=star_color)
 
     root.mainloop()
 
 
-draw_star('#b280ff', 'red')
+draw_star('#b280ff', 'red', 'black')
